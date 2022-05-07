42d2792590adbec5da94d91481916695c10a7fb3
chrispobrien <chris.obrien57@gmail.com>
2022-05-07 16:07:02 -0400

    5.5.8

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,7 +63,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -161,18 +161,22 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
-/*  activate: function(event) {
-    console.log("activate", this);
+  activate: function(event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event) {
-    console.log("out", event.target);
-  }, */
+    $(event.target).removeClass("dropover-active");
+    $(".bottom-trash").removeClass("bottom-trash-active");
+  },
   update: function(event) {
     let tempArr = [];
     $(this).children().each( function() {
@@ -221,6 +225,12 @@ var auditTask = function(taskEl) {
     $(taskEl).addClass("list-group-item-warning");
   }
 }
+
+setInterval(function() {
+  $(".card .list-group-item").each(function(index,el) {
+    auditTask(el);
+  })
+}, 1000*60*30);
 
 // load tasks for the first time
 loadTasks();
