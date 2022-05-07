fac9c97e42bd296b3ad08a44180ab8957fadefbd
chrispobrien <chris.obrien57@gmail.com>
2022-05-07 13:22:20 -0400

    5.3.8

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,7 +143,53 @@ $(".list-group").on("blur","input[type='text']", function() {
   
 })
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+/*  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  }, */
+  update: function(event) {
+    let tempArr = [];
+    $(this).children().each( function() {
+      let text = $(this).find("p").text().trim();
+      let date = $(this).find("span").text().trim();
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    let arrName = $(this).attr("id").replace("list-","");
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
 
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    //console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    //console.log("over");
+  },
+  out: function(event, ui) {
+    //console.log("out");
+  }
+});
 
 // load tasks for the first time
 loadTasks();
